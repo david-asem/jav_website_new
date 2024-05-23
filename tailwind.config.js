20dc530b410021ad6d73f5c9f0c5dcbb07914f92
David Asem <ddy.asem@gmail.com>
2024-05-23 11:38:59 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./view/**/*.hbs"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'satoshi': ['Satoshi', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
